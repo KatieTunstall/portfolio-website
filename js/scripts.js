55b52a44063436b8ca3d7557f4cd17505a28ad92
KatieTunstall <katietunstall@icloud.com>
2021-04-03 13:03:17 +0800

    Changes made for Task 2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 console.log("Hi, and welcome to my portfolio!");
+console.log("Hello");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+}
